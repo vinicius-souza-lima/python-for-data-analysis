b2b572f95abfb7981052e8a165fb5795db51de69
Vinicius de Souza Lima <vslim@solarbr.com.br>
2025-06-04 00:12:35 -0300

    cap5 inicio

--- pandas/chap_5.py
+++ pandas/chap_5.py
@@ -3,4 +3,12 @@ import pandas as pd
 #%%
 obj = pd.Series([4,7,-5,3])
 # %%
-obj
+obj.array
+# %%
+obj2 = pd.Series([4,7,-5,3], index = ["d","b","a","c"])
+# %%
+obj2["a"]
+# %%
+obj2["d"] = 6
+# %%
+obj2[["c","a","d"]]
--- polars/chap_5.py
+++ polars/chap_5.py
@@ -4,3 +4,16 @@ import polars as pl
 obj = pl.Series([4,7,-5,3])
 # %%
 obj
+# %%
+obj2 = pl.DataFrame(obj).with_columns(
+    pl.Series(["d","b","a","c"]).alias("id")
+)
+# %%
+obj2
+# %%
+obj2.filter(pl.col("id") == "a")
+# %%
+obj2.with_columns(
+    pl.when(pl.col("id") == "d")
+    .then()
+)
